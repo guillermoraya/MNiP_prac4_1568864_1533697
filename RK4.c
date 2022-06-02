ce4724923b8e8f038f3dbcc5c87b30238d36925d
guillermoraya <38941531+guillermoraya@users.noreply.github.com>
2022-06-02 13:08:26 +0200

    WE GOT DA R4 IMPLEMENTED AND IT LOOKIN' MIGHTY FINE - LET'S GOOOOOOO
    YO YO YO YO.
    Haven't checked if it works properly yet. But it's looking swell, not gonna lie.
    This calls for a celebration :+1: :smile: :birthday: :gift: :100: :top:

--- RK4.c
+++ RK4.c
@@ -1,4 +1,4 @@
-#include math.h
+#include <math.h>
 
 
 void RK4 (double (*f1)(double, double, double, void*),
@@ -7,23 +7,46 @@ void RK4 (double (*f1)(double, double, double, void*),
 			 double t0, double tf, int n, void *prm,
 			 void *prm2)
 {
-	double x_0 = x0, y_0 = y0;
-	double alpha = prm[0];
-	double quocient = prm2[0];
-	double h = (tf - t0)/n
+	double k[5]; //K per a la dimensió 'x'
+	double l[5]; //L són com els K però per a la dimensió 'y'
+	double h = (tf-t0)/(double)n; //Amplada dels subintervals
+	double t; //Variable per al pas del temps.
+	double aux_f1,aux_f2; //Variables auxiliars per a emmagatzemar els valors de les funcions f1 i f2.
 
-	double k1[2], k2[2], k3[2], k4[2];
+	// Inicialització de variables de espai ('x' i 'y') i temps ('t').
+	*x = x0;
+	*y = y0;
+	t = t0;
 
-	for (int i = 0; i<n; i++) {
-		k1[0] = x_0;
-		k1[1] = alpha * y_0 - quocient * sin(y_0);
-		k2[0] = x_0 + h/2 * k1[0];
-		k2[1] = alpha * (y_0+h/2 * k1[1]) - quocient * sin((y_0+h/2 * k1[1]));
-		k3[0] = x_0 + h/2 * k2[0];
-		k3[1] = alpha * (y_0+h/2 * k2[1]) - quocient * sin((y_0+h/2 * k2[1]));
-		k4[0] = x_0 + h * k3[0];
-		k4[1] = alpha * (y_0+h * k3[1]) - quocient * sin((y_0+h * k3[1]));
+	// Bucle del RK4 en dues dimensions
+	for(int i = 0; i < n; i++)
+	{
+		// Actualitzem la variable 't' per a indicar el pas del temps.
+		t = t0 + i*h;
 
+		// Calculem els valors de les 'slopes' en dimensió 'x' i 'y' a punts intermitjos.
+		f1(*x,*y,t,&aux_f1);
+		k[0] = h*aux_f1;
+		f2(*x,*y,t,&aux_f2);
+		l[0] = h*aux_f2;
+
+		f1(*x+k[0]/2,*y+l[0]/2,t+h/2,&aux_f1);
+		k[1] = h*aux_f1;
+		f2(*x+k[0]/2,*y+l[0]/2,t+h/2,&aux_f2);
+		l[1] = h*aux_f2;
+
+		f1(*x+k[1]/2,*y+l[1]/2,t+h/2,&aux_f1);
+		k[2] = h*aux_f1;
+		f2(*x+k[1]/2,*y+l[1]/2,t+h/2,&aux_f2);
+		l[2] = h*aux_f2;
+
+		f1(*x+k[2],*y+l[2],t+h,&aux_f1);
+		k[3] = h*aux_f1;
+		f2(*x+k[2],*y+l[2],t+h,&aux_f2);
+		l[3] = h*aux_f2;
+
+		// ... i calculem els valors aproximats de 'x' i 'y' per al temps actual
+		*x += (k[0] + 2*k[1] + 2*k[2] + k[3])/6;
+		*y += (l[0] + 2*l[1] + 2*l[2] + l[3])/6;
 	}
-	return;
 }
